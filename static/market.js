32ddc07d2c60c9cf7506798ec69746a4d2362ca6
mnl005 <mnl005@naver.com>
2023-07-23 19:50:54 +0900

    asdf

--- static/market.js
+++ static/market.js
@@ -145,16 +145,12 @@ function product_img() {
   $(".product_info1_form_other_content>div").each(function () {
     $(this).height($(this).width());
   });
-  
-
-} 
+}
 
 $(document).ready(function () {
-
   product_img();
   img_view();
   nav_view();
-
 
   //베너순환
   let currentItem = $(".product_left>div:first").show();
@@ -172,7 +168,7 @@ $(document).ready(function () {
     currentItem1 = currentItem1.next().length
       ? currentItem1.next()
       : $(".product_left1>div:first");
-      currentItem1.show();
+    currentItem1.show();
   }, 3000);
 
   $(window).resize(function () {
@@ -199,18 +195,22 @@ $(document).ready(function () {
     console.log("event_data : " + json.event_data);
     switch (json.event_type) {
       case "nav":
-        $(".nav_com").css("display","none");
-        $("#" + json.event_data).css("display","block");
+        $(".nav_com").css("display", "none");
+        $("#" + json.event_data).css("display", "block");
         product_img();
-      break;
+        break;
       case "pop":
         $(this).closest(".pop").slideToggle(500);
         break;
       case "delete":
-        $(this).closest(".delete").slideToggle().delay(1000).queue(function(next) {
-          $(this).closest(".delete").remove();
-          next();
-        });     
+        $(this)
+          .closest(".delete")
+          .slideToggle()
+          .delay(1000)
+          .queue(function (next) {
+            $(this).closest(".delete").remove();
+            next();
+          });
         break;
       case "open":
         $("#" + json.event_data).slideToggle(500);
@@ -259,82 +259,137 @@ $(document).ready(function () {
         break;
       case "registration_form_button":
         $("#product_make_form").stop().slideToggle();
-      break;
+        break;
       case "product_make_form_toggle":
         let element = $(this);
-     
-        let rotateValue = isRotated ? '0turn' : '1turn';
-        $(this).animate({
-          rotate: rotateValue
-        }, {
-          duration: 400,
-          easing: 'easeInOutQuad',
-          step: function(now) {
-            element.css('transform', 'rotate(' + now + 'deg)');
-          },
-          complete: function() {
-            isRotated = !isRotated;
-          }
-        });
-        
-        $("#product_make_form_type1").slideToggle();
-        $("#product_make_form_type2").slideToggle().css("display","flex");
 
-      break;
+        let rotateValue = isRotated ? "0turn" : "1turn";
+        $(this).animate(
+          {
+            rotate: rotateValue,
+          },
+          {
+            duration: 400,
+            easing: "easeInOutQuad",
+            step: function (now) {
+              element.css("transform", "rotate(" + now + "deg)");
+            },
+            complete: function () {
+              isRotated = !isRotated;
+            },
+          }
+        );
+
+        $("#product_make_form_type1").slideToggle();
+        $("#product_make_form_type2").slideToggle().css("display", "flex");
+
+        break;
       case "product_make_form_button1":
-      if(json.event_data === 'right' && product_make_form_button_index1 < 11){
-        $('#product_make_form_select1').children().eq(product_make_form_button_index1).stop().slideToggle();
-        $('#product_make_form_select1').children().eq(product_make_form_button_index1 + 1).stop().slideToggle();
-        product_make_form_button_index1 +=1;
-        console.log(product_make_form_button_index1);
-      }
-      else if(json.event_data === 'left' && product_make_form_button_index1 > 0){
-        $('#product_make_form_select1').children().eq(product_make_form_button_index1).stop().slideToggle();
-        $('#product_make_form_select1').children().eq(product_make_form_button_index1 - 1).stop().slideToggle();
-        product_make_form_button_index1 -=1;
-        console.log(product_make_form_button_index1);
-      }
-      break;
+        if (
+          json.event_data === "right" &&
+          product_make_form_button_index1 < 11
+        ) {
+          $("#product_make_form_select1")
+            .children()
+            .eq(product_make_form_button_index1)
+            .stop()
+            .slideToggle();
+          $("#product_make_form_select1")
+            .children()
+            .eq(product_make_form_button_index1 + 1)
+            .stop()
+            .slideToggle();
+          product_make_form_button_index1 += 1;
+          console.log(product_make_form_button_index1);
+        } else if (
+          json.event_data === "left" &&
+          product_make_form_button_index1 > 0
+        ) {
+          $("#product_make_form_select1")
+            .children()
+            .eq(product_make_form_button_index1)
+            .stop()
+            .slideToggle();
+          $("#product_make_form_select1")
+            .children()
+            .eq(product_make_form_button_index1 - 1)
+            .stop()
+            .slideToggle();
+          product_make_form_button_index1 -= 1;
+          console.log(product_make_form_button_index1);
+        }
+        break;
       case "product_make_form_button2":
-      if(json.event_data === 'right' && product_make_form_button_index2 < 5){
-        $('#product_make_form_select2').children().eq(product_make_form_button_index2).stop().slideToggle();
-        $('#product_make_form_select2').children().eq(product_make_form_button_index2 + 1).stop().slideToggle();
-        product_make_form_button_index2 +=1;
-        console.log(product_make_form_button_index2);
-      }
-      else if(json.event_data === 'left' && product_make_form_button_index2 > 0){
-        $('#product_make_form_select2').children().eq(product_make_form_button_index2).stop().slideToggle();
-        $('#product_make_form_select2').children().eq(product_make_form_button_index2 - 1).stop().slideToggle();
-        product_make_form_button_index2 -=1;
-        console.log(product_make_form_button_index2);
-      }
-    break;
+        if (
+          json.event_data === "right" &&
+          product_make_form_button_index2 < 5
+        ) {
+          $("#product_make_form_select2")
+            .children()
+            .eq(product_make_form_button_index2)
+            .stop()
+            .slideToggle();
+          $("#product_make_form_select2")
+            .children()
+            .eq(product_make_form_button_index2 + 1)
+            .stop()
+            .slideToggle();
+          product_make_form_button_index2 += 1;
+          console.log(product_make_form_button_index2);
+        } else if (
+          json.event_data === "left" &&
+          product_make_form_button_index2 > 0
+        ) {
+          $("#product_make_form_select2")
+            .children()
+            .eq(product_make_form_button_index2)
+            .stop()
+            .slideToggle();
+          $("#product_make_form_select2")
+            .children()
+            .eq(product_make_form_button_index2 - 1)
+            .stop()
+            .slideToggle();
+          product_make_form_button_index2 -= 1;
+          console.log(product_make_form_button_index2);
+        }
+        break;
       case "product_make_form_text":
-        let text_option = $("#product_make_form_select1> div:visible").data("json").data;
+        let text_option = $("#product_make_form_select1> div:visible").data(
+          "json"
+        ).data;
         console.log("text_option ::: " + text_option);
         let text_value = $("#product_make_form_select1_value").val();
         console.log("text_value ::: " + text_value);
-        let div_text = $("<div>").addClass(text_option).addClass("delete").text(text_value);
+        let div_text = $("<div>")
+          .addClass(text_option)
+          .addClass("delete")
+          .text(text_value);
         div_text.append(`
         <div class="button delete_button"
         data-json='{"event_type": "delete", "event_data": "none", "url": "none", "type": "none", "data": "none"}'
         >삭제</div>
         `);
         $(".product_detail_explanation_ex").append(div_text);
-      break;
+        break;
       case "product_make_form_img":
-   
-      let img_option = $("#product_make_form_select2> div:visible").data("json").data;
+        let img_option = $("#product_make_form_select2> div:visible").data(
+          "json"
+        ).data;
 
-        if(img_option !== 'exp_img_auto_l' && img_option !== 'exp_img_auto_m' && img_option !== 'exp_img_auto_s'){
+        if (
+          img_option !== "exp_img_auto_l" &&
+          img_option !== "exp_img_auto_m" &&
+          img_option !== "exp_img_auto_s"
+        ) {
           console.log("text_option ::: " + img_option);
-          let img_view = $('#img_view');
+          let img_view = $("#img_view");
           let firstChild = img_view.children().first();
-          let backgroundImage = firstChild.css('background-image');
-          
+          let backgroundImage = firstChild.css("background-image");
+
           let div_img = $("<div>", {
             class: img_option + " delete",
-            style: "background-image: " + backgroundImage
+            style: "background-image: " + backgroundImage,
           });
           div_img.append(`
           <span class="button delete_button"
@@ -343,13 +398,13 @@ $(document).ready(function () {
           `);
           $(".product_detail_explanation_ex").append(div_img);
           product_img();
-        }else{
+        } else {
           console.log("asdf");
           console.log("text_option ::: " + img_option);
-          
+
           let div_img = $("<div>").addClass(img_option).addClass("delete");
-          let img_view = $('#img_view');
-          img_view.children().each(function() {
+          let img_view = $("#img_view");
+          img_view.children().each(function () {
             const cloneElement = $(this).clone();
             div_img.append(cloneElement);
           });
@@ -362,39 +417,211 @@ $(document).ready(function () {
           product_img();
         }
 
-      break;
-      case "prdouct_add_done":
+        break;
+      case "prdouct_info_done":
+        let product_array = [];
+        $(".registration_product_info_option_body_one").each(function () {
+          let pair = {};
+          pair[$(this).find(".input1").val()] = $(this).find(".input2").val();
+          product_array.push(pair);
+        });
+        console.log(product_array);
 
-        const childrenHTML = $('.product_detail_explanation_ex').children().map(function() {
-          return $(this)[0].outerHTML;
-        }).get().join('');
-        console.log(childrenHTML);
-
-        let product_data = {}
-        $('.input').each(function() {
-          let name = $(this).attr('name');
+        let product_data = {};
+        $(".input").each(function () {
+          let name = $(this).attr("name");
           let value = $(this).val();
           product_data[name] = value;
         });
         console.log(product_data);
-      break;
-      case "dot":
-        $(this).closest(".registration_dot").children(".registration_dot_box").find(".dot-bubble").removeClass("dot-bubble");
-        $(this).closest(".registration_dot").children(".registration_dot_box").css({
-          "background-color": "rgb(83, 109, 255);",
-          "color": "white"
+
+        if (
+          $(".input").filter(function () {
+            return $(this).val() == "";
+          }).length > 0
+        ) {
+          $(".wornning2").stop().slideToggle();
+          setTimeout(() => {
+            $(".wornning2").stop().slideToggle();
+          }, 2000);
+        } else if (
+          $(".input1").filter(function () {
+            return $(this).val() == "";
+          }).length > 0
+        ) {
+          $(".wornning3").stop().slideToggle();
+          setTimeout(() => {
+            $(".wornning3").stop().slideToggle();
+          }, 2000);
+        } else if (
+          $(".input2").filter(function () {
+            return $(this).val() == "";
+          }).length > 0
+        ) {
+          $(".wornning3").stop().slideToggle();
+          setTimeout(() => {
+            $(".wornning3").stop().slideToggle();
+          }, 2000);
+        } else {
+          alert("상품 정보 정상! 다음 단계로 이동하세요");
+        }
+
+        $.each(product_data, function (key, value) {
+          $("#" + key).text(value);
         });
-          
-      $(this).css({
-        "background-color": "white",
-        "color": "rgb(83, 109, 255)"
-      });
-        
+        $("#registration_product_info_option_view").empty();
+        $.each(product_array, function (index, obj) {
+          $.each(obj, function (key, value) {
+            let html = `<div class="registration_product_info_option_body_one1">
+                        <div>${key}</div>
+                        <div>${value}</div>
+                      </div>`;
+            $("#registration_product_info_option_view").append(html);
+          });
+        });
+
+        break;
+      case "dot":
+        $(this)
+          .closest(".registration_dot")
+          .children(".registration_dot_box")
+          .find(".dot-bubble")
+          .removeClass("dot-bubble");
+        $(this)
+          .closest(".registration_dot")
+          .children(".registration_dot_box")
+          .css({
+            "background-color": "rgb(83, 109, 255);",
+            color: "white",
+          });
+
+        $(this).css({
+          "background-color": "white",
+          color: "rgb(83, 109, 255)",
+        });
+
         $(this).children(".dot-bubbles").addClass("dot-bubble");
-      break;
-     
-     
-     
+        break;
+      case "timeline":
+        let index = json.event_data - 1;
+
+        let index_bool1 = $("#timeline")
+          .children(".section")
+          .eq(index + 1)
+          .hasClass("filled");
+        let index_bool2 = $("#timeline")
+          .children(".section")
+          .eq(index + 2)
+          .hasClass("filled");
+        let index_bool3 = $("#timeline")
+          .children(".section")
+          .eq(index - 1)
+          .hasClass("filled");
+
+        console.log("index : " + index);
+        $("#registration_in>div").stop().hide();
+
+        if (index === 0) {
+          $("#registration_product_info").stop().show();
+        } else if (index === 1) {
+          $(".product_detail_explanation").stop().show();
+        } else if (index === 2) {
+          $("#registration_setting").stop().show();
+        } else if (index === 3) {
+          $("#registration_done").stop().show();
+        }
+
+        if (!index_bool1 && index_bool3) {
+          $("#timeline")
+            .children(".section")
+            .eq(index)
+            .removeClass("unfilled")
+            .addClass("filled");
+        } else if (!index_bool2 && index_bool3) {
+          $("#timeline")
+            .children(".section")
+            .eq(index + 1)
+            .removeClass("filled")
+            .addClass("unfilled");
+        } else if (index === 0 && !index_bool2) {
+          $("#timeline")
+            .children(".section")
+            .eq(index + 1)
+            .removeClass("filled")
+            .addClass("unfilled");
+          $("#registration_form").stop().show();
+          $("#registration_in").stop().show();
+        } else {
+          $(".wornning1").stop().slideToggle();
+          setTimeout(() => {
+            $(".wornning1").stop().slideToggle();
+          }, 2000);
+        }
+
+        break;
+      case "prdouct_info_add":
+        $(".registration_product_info_option_body").append(`
+      <div class="registration_product_info_option_body_one">
+      <input class="input1" type="text">
+      <input class="input2" type="text"  value="0">
+    </div>
+      `);
+        break;
+      case "prdouct_info_del":
+        $(
+          ".registration_product_info_option_body .registration_product_info_option_body_one:last"
+        ).remove();
+        break;
+      case "registration_done":
+        let info = {};
+
+        info.brand = $("#brand").text();
+        info.name = $("#name").text();
+        info.price = $("#price").text();
+        info.del = $("#del").text();
+        info.change = $("#change").text();
+        info.ban = $("#ban").text();
+        info.per = $("#per").text();
+        info.make_won = $("#make_won").text();
+        info.make_su = $("#make_su").text();
+        info.cus = $("#cus").text();
+
+        console.log(info);
+
+        let array = [];
+
+        $(
+          "#registration_product_info_option_view .registration_product_info_option_body_one1"
+        ).each(function () {
+          let pair = {};
+          pair[$(this).children("div").first().text()] = $(this)
+            .children("div")
+            .last()
+            .text();
+          array.push(pair);
+        });
+
+        console.log(array);
+
+        const childrenHTML = $(".product_detail_explanation_ex")
+        .children()
+        .map(function () {
+            // clone the current element
+            let clone = $(this).clone();
+            // remove .delete_button from the clone
+            clone.find('.delete_button').remove();
+            // return the outerHTML of the clone
+            return clone[0].outerHTML;
+        })
+        .get()
+        .join("");
+    console.log(childrenHTML);
+    
+
+      
+
+
+        break;
       default:
         console.log("이벤트타입 없음");
         break;
@@ -418,7 +645,6 @@ $(document).ready(function () {
   let lastScrollTop = 0;
 
   $(document).scroll(function () {
-
     let currentScrollTop = $(this).scrollTop();
     // console.log(currentScrollTop);
     // console.log(lastScrollTop);
@@ -441,17 +667,17 @@ $(document).ready(function () {
   });
 });
 
-function img_view(){
-  $('#img_add').change(function() {
-    $('#img_view').empty();
+function img_view() {
+  $("#img_add").change(function () {
+    $("#img_view").empty();
     const files = Array.from($(this)[0].files);
-    const imgView = $('#img_view');
-  
+    const imgView = $("#img_view");
+
     files.forEach((file) => {
       const reader = new FileReader();
       reader.onload = (e) => {
         const imageUrl = e.target.result;
-        const div = $('<div>').css('background-image', `url(${imageUrl})`);
+        const div = $("<div>").css("background-image", `url(${imageUrl})`);
         imgView.append(div);
       };
       reader.readAsDataURL(file);
